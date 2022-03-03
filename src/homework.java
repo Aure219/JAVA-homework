85b5990147e41b603486ac3a0ebeb42e2b113623
Aure219 <1471665755@qq.com>
2022-03-03 16:14:45 +0800

    1

---
+++ src/homework.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;//如果要用Scanner,要加入这个
+public class homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		double radius;
+//		double area;
+//		radius=20;
+//		area=radius*radius*3.14159;
+//		System.out.println("The area for the circle of radius"+radius+" is "+area);
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Enter a number for radius: ");
+//		double radius=input.nextDouble();
+//		double area=radius*radius*3.14159;
+//		System.out.println("The area for the circle of radius "+radius+" is "+area);
+//		Scanner input = new Scanner(System.in);
+//		double number1 = input.nextDouble();
+//		double number2 = input.nextDouble();
+//		double number3 = input.nextDouble();
+//		double average=(number1+number2+number3)/3;
+//		System.out.println("The average is"+average);
+		
+//		final double pi = 3.14159;
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Enter a number for radius: ");
+//		double radius=input.nextDouble();
+//		double area = radius*radius*pi;
+//		System.out.println("The area for the circle of radius "+radius+" is "+area);
+		
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Enter an integer for second: ");
+//		int second = input.nextInt();
+//		int minutes = second/60;
+//		int remainSeconds = second%60;
+//		System.out.println(second+"second is "+minutes+"minutes and"+remainSeconds+"second");
+		
+//		long totalMilliseconds = System.currentTimeMillis();
+//		long totalSecond = totalMilliseconds/1000;
+//		long currentSecond = totalSecond%60;
+//		long totalMinutes = totalSecond/60;
+//		long totalHour = totalMinutes/60;
+//		long currentHour = totalHour%24;
+//		System.out.println("Current times is"+currentHour+" "+totalMinutes+":"+currentSecond);
+		
+	//System.out.println("Programming in Java is fun!");
+		
+//		Scanner input = new Scanner(System.in);
+//        double purchaseAmount = input.nextDouble();
+//        double tax = purchaseAmount * 0.06;
+//        System.out.println("Sales tax is $" + (int) (tax * 100) / 100.0);
+		
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter annual interest rate ,e.g.,7.25: ");
+//        double annualInterestRate = input.nextDouble();
+//        double monthlyInerestRate = annualInterestRate / 1200;
+//        System.out.print("Enter number of years as an integer , e.g. 5: ");
+//        int numberOfYears = input.nextInt();
+//        System.out.print("Enter loan amount, e.g.,120000.95: ");
+//        double loanAmount = input.nextDouble();
+//        double monthlyPayment = loanAmount * monthlyInerestRate
+//                / (1 - 1 / Math.pow(1 + monthlyInerestRate, numberOfYears * 12));
+//        double totalPayment = monthlyPayment * numberOfYears * 12;
+//        System.out.println("The monthly payment is $" 
+//                            + (int) (monthlyPayment * 100) / 100);
+//        System.out.println("The total payment is $" 
+//                                + (int) (totalPayment * 100) / 100);
+		
+//		  Scanner input = new Scanner(System.in);
+//	        System.out.println("Enter an amount in double ,for example 11.56: ");
+//	        double amount = input.nextDouble();
+//	        int remainingAmount = (int) (amount * 100);
+//	        int numberOfOneDollars = remainingAmount / 100;
+//	        remainingAmount = remainingAmount % 100;
+//	        int numberOfQuarters = remainingAmount / 25;
+//	        remainingAmount = remainingAmount % 25;
+//	        int numberOfDimes = remainingAmount / 10;
+//	        remainingAmount = remainingAmount % 10;
+//	        int numberOfNickels = remainingAmount / 5;
+//	        remainingAmount = remainingAmount % 5;
+//	        int numberOfPennies = remainingAmount;
+//	        System.out.println("Your amount " + amount + " consists of");
+//	        System.out.println(" " + numberOfOneDollars + " dollars");
+//	        System.out.println(" " + numberOfQuarters + " quarters");
+//	        System.out.println(" " + numberOfDimes + " dimes");
+//	        System.out.println(" " + numberOfNickels + " nickels");
+//	        System.out.println(" " + numberOfPennies + " pennies");
+	    
+
+        
+        
+		Scanner input = new Scanner(System.in);
+		double f1 = input.nextDouble();
+		int zheng = input.nextInt();
+		char c = input.next().charAt(0);
+		double f2 = input.nextDouble();
+
+		
+	}
+
+}
