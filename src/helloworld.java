62e164c88d86e284b0e02072f8e1cf4aa13e2c3c
Aure219 <1471665755@qq.com>
2022-03-03 16:11:13 +0800

    Java

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,6 +1,12 @@
 
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!");
+		System.out.println("Welcome to java!");
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+		System.out.print("(10.5+2*3)/(45-3.5) = ");//print显示完后不会移到下一行
+		System.out.println((10.5+2*3)/(45-3.5));
+		
 	}
 }
